506e011c24e0009aa63a782dce74b29a4ee85ee7
Jacek Dziewanowski <dziewanowski@email.com>
2025-12-08 15:26:00 +0100

    ci: logs-on-host

---
+++ app/lib/logger.ts
@@ -0,0 +1,58 @@
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+import path from 'path';
+import util from 'util';
+
+const logDir = path.join(process.cwd(), 'logs');
+
+const fileRotateTransport = new winston.transports.DailyRotateFile({
+  filename: 'app-%DATE%.log',
+  dirname: logDir,
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '31d',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  )
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    fileRotateTransport,
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.printf(({ level, message }) => {
+          return `${level}: ${String(message)}`;
+        })
+      )
+    })
+  ]
+});
+
+// Override console methods to capture all logs
+console.log = (...args: unknown[]) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  logger.info(util.format(...args));
+};
+
+console.error = (...args: unknown[]) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  logger.error(util.format(...args));
+};
+
+console.warn = (...args: unknown[]) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  logger.warn(util.format(...args));
+};
+
+console.info = (...args: unknown[]) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  logger.info(util.format(...args));
+};
+
+export default logger;
+
--- app/server/db.ts
+++ app/server/db.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { env } from "@root/app/env";
+import "@root/app/lib/logger"; // Initialize logger
 
 const createPrismaClient = () =>
   new PrismaClient({
